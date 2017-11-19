fe01964303e3ef065d2525e61539a311da5586b9
Karsten <balogh.karsten@gmail.com>
2017-11-19 16:48:45 +0100

    Looking better

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ curl -X POST -H "Content-Type: application/json" -d {
   "greeting":{
     "text":"Hello, I'm the Yuranotbot. Lets learn to code together ;)"
   }
-} "https://graph.facebook.com/v2.6/me/thread_settings?access_token=PAGE_ACCESS_TOKEN"
+} "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAACmHUn9r7sBAIT0nNkTWmZAwuJZCZATUTcznZA235WPOH8ZBTZA7cBeUpps6vQcX4D4RhMguByuTv0A48StZBWoyuW9YgTOJa2bklIhTKWQeIpVmc5ri2QmmBfF8KeuqKXO3kc5xxW1m4HMSBxAXYe4UfZA7Uyoj8LlNgJQVoHlyza08cPJFio4"
 
 function receivedMessage(event) {
   var senderID = event.sender.id;
