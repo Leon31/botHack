59e1738c21e78660f7d216759067f0654784e082
Karsten <balogh.karsten@gmail.com>
2017-11-19 14:39:03 +0100

    Testing quest() message generation 8

--- questObj.js
+++ questObj.js
@@ -9,7 +9,9 @@ var arrOfQuest = [
     title:["var arr = [1,2,3]", "arr[1][2][3]", "var arr = {1,2,3}"],
     payload: ["true ", "false Global_Objects/Array", "false Global_Objects/Array"]
   },
-  { quest: '```var obj1 = {}; var obj2 = {};``` What is the value of (obj1 === obj2)?',
+  { quest: '```
+  var obj1 = {}; var obj2 = {};
+  ``` What is the value of (obj1 === obj2)?',
     title: [ 'true', 'false' ],
     payload: [ "false Operators/Comparison_Operators#Identity", "true" ]
   }
